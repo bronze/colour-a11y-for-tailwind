37cbaaa808306e861d8b3d91d26a6766618855ff
Carlos Bronze <170220+bronze@users.noreply.github.com>
2025-06-06 13:32:09 -0300

    vercel analytics on pages router

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
+import {Analytics} from '@vercel/analytics/next';
 import "../styles/index.css"
 
 function MyApp({Component, pageProps}) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />;
+    </>
+  );
 }
 
 export default MyApp
