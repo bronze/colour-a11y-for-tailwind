d1afb120bf6e6464501b0e75a152ca6d18245ed6
Carlos Bronze <170220+bronze@users.noreply.github.com>
2025-06-06 11:14:16 -0300

    prettier

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Colour Accessibility for Tailwind CSS
 
 Checks the colours using the WCAG contrast ratings on both white and black backgrounds
+
+Forked from: https://github.com/samrobbins85/colour-a11y-for-tailwind
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,20 @@
-const defaultTheme = require("tailwindcss/defaultTheme");
-module.exports = {
-	darkMode: "class",
-	content: [
-		"./components/**/*.{js,ts,jsx,tsx}",
-		"./pages/**/*.{js,ts,jsx,tsx}",
-	],
-	theme: {
-		extend: {
-			fontFamily: {
-				sans: ["Inter var", ...defaultTheme.fontFamily.sans],
-			},
-			maxWidth: {
-				ch64: "64ch",
-			},
-		},
-	},
-	variants: {},
-	plugins: [require("@tailwindcss/typography")],
+const defaultTheme=require("tailwindcss/defaultTheme");
+module.exports={
+  darkMode: "class",
+  content: [
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+      },
+      maxWidth: {
+        ch64: "64ch",
+      },
+    },
+  },
+  variants: {},
+  plugins: [require("@tailwindcss/typography")],
 };
