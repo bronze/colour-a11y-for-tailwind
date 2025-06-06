b987e0d32daa63e29cb00899ad8106caef3a041c
Carlos Bronze <170220+bronze@users.noreply.github.com>
2025-06-06 13:02:04 -0300

    svg warnings

--- pages/about.jsx
+++ pages/about.jsx
@@ -18,11 +18,11 @@ export default function About() {
             fill="none"
             viewBox="0 0 24 24"
             stroke="currentColor"
-            class="h-10 w-10 inline-block text-gray-600">
+            className="h-10 w-10 inline-block text-gray-600">
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
               d="M11 15l-3-3m0 0l3-3m-3 3h8M3 12a9 9 0 1118 0 9 9 0 01-18 0z"
             />
           </svg>
